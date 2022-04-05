5bb34aa95efca5a939aafefc61f131d441db1ac7
fozata <furkan.ozata@hotmail.com>
2022-04-05 16:01:37 +0300

    second

--- form.js
+++ form.js
@@ -1,8 +1,13 @@
 chrome.runtime.onMessage.addListener(function (sender, sendResponse) {
-  setTimeout(() => {
-    document.getElementById("btnNewApplication").click();
-  }, 1000);
-  setTimeout(() => {
-    document.getElementById("btnNext").click();
-  }, 11000);
+  try {
+    setTimeout(() => {
+      document.getElementById("btnNewApplication").click();
+    }, 1000);
+    setTimeout(() => {
+      document.getElementById("btnNext").click();
+    }, 11000);
+  } catch (error) {
+    console.log(error);
+    sendResponse({ status: "Exception accurrend!" });
+  }
 });
